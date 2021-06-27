3472757019d3f2c217d585340f8a9141c41d44b2
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-27 17:14:47 +0600

    server.js file initated

---
+++ Timestamp Project/server.js
@@ -0,0 +1,19 @@
+/*
+ * Title: Timestamp Microservice
+ * Description: It will print timestamp in an exact endpoint
+ * Author: Mohammad Mesbaul Haque
+ * Github link: https://github.com/mohammad-mesbaul-haque
+ * Date: 27/06/2021
+ */
+ 
+// Dependencies.
+ 
+ 
+// App object or Module scaffolding.
+ 
+// main functions or objects.
+ 
+ 
+ 
+ 
+// export the module.
