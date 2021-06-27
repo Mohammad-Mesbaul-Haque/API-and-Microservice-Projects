7a1c2ecbbe1cf98ca4a5598c76e69d4508c4a3bf
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-06-27 17:21:03 +0600

    server created

--- Timestamp Project/server.js
+++ Timestamp Project/server.js
@@ -7,13 +7,21 @@
  */
  
 // Dependencies.
- 
+ const express = require('express');
  
 // App object or Module scaffolding.
+const app = express()
  
 // main functions or objects.
+ app.get('/', (req, res)=>{
+     res.json({"ok" : "yes!"})
+ })
  
  
- 
+
+ // listening server
+ app.listen(3000, ()=>{
+     console.log(`listening timestamp app`);
+ })
  
 // export the module.
