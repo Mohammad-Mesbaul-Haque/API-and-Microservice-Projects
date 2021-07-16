0f5eedda0ac6806feb1593556ee9ab33efe8e27b
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-07-17 00:57:10 +0600

    done and working :)

--- Timestamp Project/server.js
+++ Timestamp Project/server.js
@@ -5,33 +5,45 @@
  * Github link: https://github.com/mohammad-mesbaul-haque
  * Date: 27/06/2021
  */
- 
-// Dependencies.
- const express = require('express');
- 
-// App object or Module scaffolding.
-const app = express()
- 
-// main functions or objects.
- app.get('/', (req, res)=>{
-     res.json({"ok" : "yes!"})
- })
- 
- // project's main endpoint
- let responseObject = {}
- app.get('/api/timestamp/:input', (req, res)=>{
-     let input = req.params.input;
+const express = require("express");
+const app = express();
 
-     if (input.includes('-')) {
-         responseObject.unix = new Date(input).getTime();
-     }
+const cors = require("cors");
+app.use(cors()); 
 
-     res.json(responseObject);
- })
 
- // listening server
- app.listen(3000, ()=>{
-     console.log(`listening timestamp app`);
- })
- 
+// index
+app.get("/", function (req, res) {
+  res.send('index page');
+});
+
+// timestamp API
+app.get("/api/:date?", (req, res) => {
+  const givenDate = req.params.date;
+  let date;
+
+  // check if no date provided
+  if (!givenDate) {
+    date = new Date();
+  } else {
+    // check if unix time:
+    //    number string multiplied by 1 gives this number, data string gives NaN
+    const checkUnix = givenDate * 1;
+    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
+  }
+
+  //check if valid format
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix, utc });
+  }
+});
+
+app.listen(3000, ()=>{
+  console.log('Listening on port 3000');
+})
+
 // export the module.
