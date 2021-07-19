858326f3fe603599d01524c250003e4cc16e5e21
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-07-19 16:27:22 +0600

    free code version added just for further practice or analysis

--- URL-Shortner-api/server.js
+++ URL-Shortner-api/server.js
@@ -88,3 +88,92 @@ app.listen(process.env.PORT, ()=>{
  
 // export the module.
  
+
+// free code version
+// require('dotenv').config();
+// const express = require('express');
+// const dns = require('dns')
+// const urlParser = require('url');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const mongoose = require('mongoose')
+// const app = express();
+
+
+// // connection with mongodb
+// mongoose.connect(process.env.str, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(()=>{
+//     console.log(`Connection successful`);
+// })
+// .catch((e)=>{
+//     console.log(e);
+// })
+
+
+// // Creating a schema for data
+// const schema = mongoose.Schema({url: String});
+// const Url = mongoose.model('Url', schema);
+
+// // apps permissions
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(cors());
+
+// // Basic Configuration
+// const port = process.env.PORT || 3000;
+
+// app.use(cors());
+
+// app.use('/public', express.static(`${process.cwd()}/public`));
+
+// app.get('/', function(req, res) {
+//   res.sendFile(process.cwd() + '/views/index.html');
+// });
+
+// // Your first API endpoint
+// app.get('/api/hello', function(req, res) {
+//   res.json({ greeting: 'hello API' });
+// });
+
+
+// // My endpoint post
+// app.post('/api/shorturl', (req, res)=>{
+//     const bodyurl = req.body.url;
+
+//     const parser = dns.lookup(urlParser.parse(bodyurl).hostname, (error, address)=>{
+//         if (!address) {
+//             res.json({"error": "Invalid URL"})
+//         } else{
+//             const url = new Url({url: bodyurl})
+//             url.save((err, data)=>{
+//                 res.json({
+//                     original_url : data.url,
+//                     short_url : data.id
+//                 })
+//             })
+//         }
+//     }) 
+
+// })
+
+
+
+// // get and redirect url
+// app.get('/api/shorturl/:short_url', (req, res)=>{
+//     const id = req.params.short_url;
+//     Url.findById(id, (err, data)=>{
+//         if (!data) {
+//             res.json({error: "invalid URL"})
+//         } else {
+//             res.redirect(data.url);
+//         }
+//     })
+// })
+
+
+
+// app.listen(port, function() {
+//   console.log(`Listening on port ${port}`);
+// });
